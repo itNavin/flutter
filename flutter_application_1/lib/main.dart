f3a9e6f47cad1fd6cc36b00966b2769b0df9827b
Navin <ITdansaikul@gmail.com>
2024-02-22 11:42:53 +0700

    [add]

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Mylayout demo'),
+          ),
+          body: Center(
+            child: buildRow(),
+          ),
+        ));
+  }
+
+  Widget buildRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('images/pic1.jpg'),
+        Image.asset('images/pic2.jpg'),
+        Image.asset('images/pic3.jpg'),
+      ],
+    );
+  }
+
+}
